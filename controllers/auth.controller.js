ad87ebbbe95480574f5b1676ef5660f62c94595e
Dawood <sulaimondawod@gmail.com>
2024-05-12 16:06:56 +0100

    added jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,15 +1,65 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-  //db operations
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
+  try {
+    //db operations
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    //Create user in database
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User created succesfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 
-export const login = (req, res) => {
-  //db operations
-  console.log("Login endpoint");
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: age,
+      }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+        // secure:true
+      })
+      .status(200)
+      .json({ message: "Login successful" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login" });
+  }
 };
 
 export const logout = (req, res) => {
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
 const app = express();
-const PORT = process.env.PORT || 8800;
+
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
+app.unsubscribe(cookieParser());
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
