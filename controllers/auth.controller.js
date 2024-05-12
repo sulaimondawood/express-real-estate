7a76f1078ad7fc8c28a763ff3bf8c790db5529b2
Dawood <sulaimondawod@gmail.com>
2024-05-12 16:16:52 +0100

    finished auth api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -63,5 +63,5 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  //db operations
+  res.clearCookie("token").status(200).json({ message: "Logout successful" });
 };
