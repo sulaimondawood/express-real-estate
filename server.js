4313d98b7c16c0e4d593d3603978a44eb4222a1e
Dawood <sulaimondawod@gmail.com>
2024-05-14 12:06:41 +0100

    new

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import "dotenv/config";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
-app.unsubscribe(cookieParser());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
